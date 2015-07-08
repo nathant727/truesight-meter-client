70994f8f421998225fb089fc6953ed7fdacb26fe
TheBigHNF <henry@boundary.com>
2015-07-08 16:07:04 -0500

    Added source field and timestamp

--- src/main/java/com/boundary/meter/client/model/Measure.java
+++ src/main/java/com/boundary/meter/client/model/Measure.java
@@ -2,13 +2,31 @@ package com.boundary.meter.client.model;
 
 import org.immutables.value.Value;
 
+import java.time.Instant;
+import java.util.Optional;
+
+import static com.google.common.base.Preconditions.checkArgument;
+
 /**
  * Immutable representation of a single measurement
  */
 @Value.Immutable
 public abstract class Measure {
-    @Value.Parameter
+
+
     public abstract String name();
-    @Value.Parameter
     public abstract double value();
+    @Value.Default
+    public Instant timestamp() {
+        return Instant.now();
+    }
+    public abstract Optional<String> source();
+
+    @Value.Check
+    protected void check() {
+        checkArgument(name().getBytes().length <= 100, "Metric name length must not be > 100 bytes");
+        source().ifPresent(m -> {
+            checkArgument(m.getBytes().length <= 100, "Source name length must not be > 100 bytes");
+        });
+    }
 }
