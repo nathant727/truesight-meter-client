5a72f36c6273241623eaa9462b6be5e6e6c7f924
Jesse Hodges <jesse@boundary.com>
2015-07-27 08:52:24 -0500

    fix visibility

--- src/main/java/com/boundary/meter/client/Json.java
+++ src/main/java/com/boundary/meter/client/Json.java
@@ -5,7 +5,9 @@ import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 
 public class Json {
 
-    public static ObjectMapper newObjectMapper() {
+    private Json(){}
+
+    private static ObjectMapper newObjectMapper() {
         final ObjectMapper mapper = new ObjectMapper();
         mapper.registerModule(new Jdk8Module());
         return mapper;
