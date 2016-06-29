1f3b5ff1098835836fb54a689f6ded080c8c8ce1
Nathan Thompson <nathant727@yahoo.com>
2016-06-29 16:05:20 -0500

    Update Measure.java
    Updated javadoc.

--- src/main/java/com/bmc/truesight/saas/meter/client/model/Measure.java
+++ src/main/java/com/bmc/truesight/saas/meter/client/model/Measure.java
@@ -22,6 +22,14 @@ public abstract class Measure {
     }
     public abstract Optional<String> source();
 
+    /**
+     * Creates a Measure using the required parameters: name and value.
+     * The timestamp for this Measure defaults to <a href="https://docs.oracle.com/javase/8/docs/api/java/time/Instant.html#now--">Instant.now()</a>.
+     * The source of the event will be the Fully Qualified Domain Name of the meter host.
+     * @param name  the name of the Metric displayed in Pulse (must not be greater than 100 characters in length).
+     * @param value the value for this Metric.
+     * @return an Immutable Measure with the name and value given.
+     */
     public static Measure of(String name, double value) {
         return ImmutableMeasure.builder()
                 .name(name)
@@ -29,6 +37,15 @@ public abstract class Measure {
                 .build();
     }
 
+    /**
+     * Creates a Measure using the required parameters: name, value and timestamp.  The source of the event will be the
+     * Fully Qualified Domain Name of the meter host.
+     *
+     * @param name      the name of the Metric displayed in Pulse (must not be greater than 100 characters in length).
+     * @param value     the value for this Metric.
+     * @param timestamp specifies the time the Measure
+     * @return an Immutable Measure with the name and value given.
+     */
     public static Measure of(String name, double value, Instant timestamp) {
         return ImmutableMeasure.builder()
                 .name(name)
@@ -37,6 +54,15 @@ public abstract class Measure {
                 .build();
     }
 
+    /**
+     * Creates a Measure using the required parameters: name, value and source.
+     * The timestamp for this Measure defaults to <a href="https://docs.oracle.com/javase/8/docs/api/java/time/Instant.html#now--">Instant.now()</a>.
+     *
+     * @param name   the name of the Metric displayed in Pulse (must not be greater than 100 characters in length).
+     * @param value  the value for this Metric.
+     * @param source the event source (must not be greater than 100 characters in length).
+     * @return an Immutable Measure with the name and value given.
+     */
     public static Measure of(String name, double value, String source) {
         return ImmutableMeasure.builder()
                 .name(name)
@@ -45,6 +71,15 @@ public abstract class Measure {
                 .build();
     }
 
+    /**
+     * Creates a Measure using the required parameters: name, value, timestamp and source.
+     *
+     * @param name      the name of the Metric displayed in Pulse (must not be greater than 100 characters in length).
+     * @param value     the value for this Metric.
+     * @param timestamp specifies the time the Measure
+     * @param source    the event source (must not be greater than 100 characters in length).
+     * @return an Immutable Measure with the name and value given.
+     */
     public static Measure of(String name, double value, Instant timestamp, String source) {
         return ImmutableMeasure.builder()
                 .name(name)
